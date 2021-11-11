5373809f147d535f0f182b762e0d93fb731a0d5d
Avokados <avokados99@gmail.com>
2021-11-11 13:12:00 +0700

    add file

---
+++ Bot_igor.py
@@ -0,0 +1,14 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+bot = Bot(token="1905808928:AAFx1uBKo20mvO_ji052zD418ovSTxxWy30")
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def get_message(message: types.Message):
+    chat_id = message.chat.id
+    text = message.text
+    send_message = await bot.send_message(chat_id=chat_id, text=text)
+    print(send_message.to_python())
+
+executor.start_polling(dp)
