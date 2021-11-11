75fddead51de06d034a5eebe856d7b47e3942069
Alvor <alvor1975@gmail.com>
2021-11-11 15:17:56 +0700

    сам ты

--- Bot_igor.py
+++ Bot_igor.py
@@ -8,7 +8,7 @@ dp = Dispatcher(bot)
 @dp.message_handler()
 async def get_message(message: types.Message):
     chat_id = message.chat.id
-    text = message.text
+    text = "сам ты "+message.text
     send_message = await bot.send_message(chat_id=chat_id, text=text)
     print(send_message.to_python())
 
